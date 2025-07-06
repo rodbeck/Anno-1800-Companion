acd7601b723b3906ab6f491a6e6817fcea7fb06d
Rodolphe Beck <rodolphe.beck@outlook.com>
2025-07-06 21:54:56 +0200

    feat: adds translation.

--- Anno 1800 CompanionUITests/Anno_1800_CompanionUITests.swift
+++ Anno 1800 CompanionUITests/Anno_1800_CompanionUITests.swift
@@ -21,18 +21,10 @@ final class Anno_1800_CompanionUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
     
     @MainActor
     func testScreenshots() throws {
+        
         
     }
 
