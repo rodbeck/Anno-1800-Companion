9c025b27d267971fb2fc245fc84ab4394359f99e
Rodolphe Beck <rodolphe.beck@outlook.com>
2025-07-06 16:31:30 +0200

    feat: adds fastlane snapshots generation.

--- Anno 1800 CompanionUITests/Anno_1800_CompanionUITests.swift
+++ Anno 1800 CompanionUITests/Anno_1800_CompanionUITests.swift
@@ -33,6 +33,49 @@ final class Anno_1800_CompanionUITests: XCTestCase {
     
     @MainActor
     func testScreenshots() throws {
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        app.activate()
+        
+        snapshot("01MainScreen")
+        
+        app/*@START_MENU_TOKEN@*/.buttons["Ajouter"]/*[[".navigationBars.buttons[\"Ajouter\"]",".buttons[\"Ajouter\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        app/*@START_MENU_TOKEN@*/.buttons.staticTexts["The Old World"].firstMatch/*[[".staticTexts.matching(identifier: \"The Old World\").element(boundBy: 1)",".buttons",".staticTexts.firstMatch",".staticTexts[\"The Old World\"].firstMatch"],[[[-1,1,1],[-1,0]],[[-1,3],[-1,2]]],[0,0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Enbesa"]/*[[".cells.buttons[\"Enbesa\"]",".buttons[\"Enbesa\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        
+        let element = app.textFields.matching(identifier: "Number").element(boundBy: 0)
+        element.tap()
+        element.typeKey(.delete, modifierFlags:[])
+        app/*@START_MENU_TOKEN@*/.textFields["300"]/*[[".otherElements.textFields[\"300\"]",".textFields[\"300\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.typeText("\"àà")
+        
+        let element2 = app/*@START_MENU_TOKEN@*/.textFields["Name"]/*[[".otherElements.textFields[\"Name\"]",".textFields[\"Name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch
+        element2.tap()
+        element2.tap()
+        app/*@START_MENU_TOKEN@*/.textFields["Name"]/*[[".otherElements",".textFields[\"Île enbes\"]",".textFields[\"Name\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.typeText("Ile enbesa")
+        app/*@START_MENU_TOKEN@*/.buttons["Save"]/*[[".otherElements.buttons[\"Save\"]",".buttons[\"Save\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Île en Elsa, 300"]/*[[".buttons",".containing(.image, identifier: \"regions\/enbesa\")",".containing(.staticText, identifier: \"300\")",".containing(.staticText, identifier: \"Île en Elsa\")",".otherElements.buttons[\"Île en Elsa, 300\"]",".buttons[\"Île en Elsa, 300\"]"],[[[-1,5],[-1,4],[-1,0,1]],[[-1,3],[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        
+        snapshot("02IslandScreen")
+        
+        let element3 = app/*@START_MENU_TOKEN@*/.buttons["Calculate"]/*[[".otherElements.buttons[\"Calculate\"]",".buttons[\"Calculate\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch
+        element3.tap()
+        element3.tap()
+        
+        let element4 = app/*@START_MENU_TOKEN@*/.buttons["BackButton"]/*[[".navigationBars",".buttons",".buttons[\"Islands\"]",".buttons[\"BackButton\"]"],[[[-1,3],[-1,2],[-1,0,1]],[[-1,3],[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch
+        element4.tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["1200"]/*[[".buttons.staticTexts[\"1200\"]",".staticTexts[\"1200\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        element3.tap()
+        
+        let cellsQuery = app.cells
+        cellsQuery/*@START_MENU_TOKEN@*/.containing(.image, identifier: "Workers").firstMatch/*[[".element(boundBy: 8)",".containing(.image, identifier: \"icons\/workforce-workers\").firstMatch",".containing(.staticText, identifier: \"Workers\").firstMatch",".containing(.image, identifier: \"Workers\").firstMatch"],[[[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
+        cellsQuery/*@START_MENU_TOKEN@*/.containing(.image, identifier: "Engineers").firstMatch/*[[".element(boundBy: 5)",".containing(.image, identifier: \"icons\/workforce-engineers\").firstMatch",".containing(.staticText, identifier: \"Engineers\").firstMatch",".containing(.image, identifier: \"Engineers\").firstMatch"],[[[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeDown()
+        element4.tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["Île en Elsa"]/*[[".buttons.staticTexts[\"Île en Elsa\"]",".staticTexts[\"Île en Elsa\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        element3.doubleTap()
+        element4.tap()
+        
+        snapshot("03IslandScreen")
     }
 
     @MainActor
